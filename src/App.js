f0e8db0a833ecd24b0113d284ad52a40e85aed5a
adistrim <adityajklu@gmail.com>
2023-04-14 13:21:06 +0530

    Version 0.1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import RouteVisualizer from './RouteVisualizer/RouteVisualizer';
+
+function App() {
+  return (
+    <div className="App">
+      <RouteVisualizer></RouteVisualizer>
+    </div>
+  );
+}
+
+export default App;
